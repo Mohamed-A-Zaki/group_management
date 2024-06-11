82b20359323b839ef93d82eeb4243bf2b01aba54
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-06-11 10:57:45 +0300

    add to cache

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,14 @@ import "./index.css";
 import router from "./routes/router.tsx";
 import { RouterProvider } from "react-router-dom";
 
+import { PlainLocalStorageDriver, setCacheConfigurations } from "@mongez/cache";
+
+setCacheConfigurations({
+  driver: new PlainLocalStorageDriver(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </>
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,31 @@
 import Groups from "@/components/home/Groups";
 import CreateGroupDialog from "@/dialogs/CreateGroupDialog";
 import EditGroupDialog from "@/dialogs/EditGroupDialog";
+import { useEffect } from "react";
+import cache from "@mongez/cache";
+import { groups_list_atom } from "@/atoms/groups_list_atom";
 
 export default function HomePage() {
+  const { data } = groups_list_atom.useValue();
+
+  useEffect(() => {
+    const data = cache.get("groups_list");
+
+    if (data) {
+      /***
+       * update the groups list
+       */
+      groups_list_atom.change("data", data);
+    }
+  }, []);
+
+  useEffect(() => {
+    /**
+     * store the groups list in the cache
+     */
+    cache.set("groups_list", data);
+  }, [data]);
+
   return (
     <>
       <Groups />
